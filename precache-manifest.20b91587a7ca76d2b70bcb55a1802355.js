46a613377aad1ef506f359b58ce2876a581a0a90
Ramesh <ramiga143@gmail.com>
2020-05-15 02:18:36 +0530

    Updates

---
+++ precache-manifest.20b91587a7ca76d2b70bcb55a1802355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98a2c2f0fe4c0871ad57",
+    "url": "/me/static/css/main.6b091556.chunk.css"
+  },
+  {
+    "revision": "98a2c2f0fe4c0871ad57",
+    "url": "/me/static/js/main.98a2c2f0.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "e897f1746520a72a49db",
+    "url": "/me/static/js/2.e897f174.chunk.js"
+  },
+  {
+    "revision": "398eac9c07277e299937547dbbd781b6",
+    "url": "/me/index.html"
+  }
+];
